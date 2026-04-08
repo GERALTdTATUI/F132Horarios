225e6d62432a1817667f64eabe48c219bb6ca296
GERALTdTATUI <nathanrheata@gmail.com>
2026-04-08 20:14:17 -0300

    Versão 3.2 - Ajustes na importação de variáveis

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ sed -e "s|#clmnHdrs#|$clmnHdrs|g" \
 -e "s|#excelLastEditedSource#|$excelLastEditedSource|g" \
 -e "s|#ignoredWordFilters#|$ignoredWordFilters|g" \
 -e "s|#ignoredExclusiveFilters#|$ignoredExclusiveFilters|g" \
-parameters.template.js > parameters.js.new
+parameters.template.js > parameters.js
 
 exit 0;
