f1437bb0a845cfd31df283ef813fd165c4181401
GERALTdTATUI <nathanrheata@gmail.com>
2026-04-08 21:07:26 -0300

    Versão 3.5 - Ajustes noscript de build .v2

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sed -e "s|\"#clmnHdrs#\"|$clmnHdrs|g" \
--e "s|\"#sources#\"|$sources|g" \
--e "s|\"#excelLastEditedSource#\"|$excelLastEditedSource|g" \
--e "s|\"#ignoredWordFilters#\"|$ignoredWordFilters|g" \
--e "s|\"#ignoredExclusiveFilters#\"|$ignoredExclusiveFilters|g" \
+sed -e "s|\"#clmnHdrs#\"|${clmnHdrs//&/\\&}|g" \
+-e "s|\"#sources#\"|${sources//&/\\&}|g" \
+-e "s|\"#excelLastEditedSource#\"|${excelLastEditedSource//&/\\&}|g" \
+-e "s|\"#ignoredWordFilters#\"|${ignoredWordFilters//&/\\&}|g" \
+-e "s|\"#ignoredExclusiveFilters#\"|${ignoredExclusiveFilters//&/\\&}|g" \
 parameters.template.js > parameters.js
 
 exit 0;
